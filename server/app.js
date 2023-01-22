2f066f0df6f76d38d4c20c9c4c8c2c3e1943d0ef
Leapfrog <lf@Leapfrogs-MacBook-Pro.local>
2023-01-22 19:14:44 +0545

    Final commit

---
+++ server/app.js
@@ -0,0 +1,49 @@
+﻿const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const indexRouter = require("./routes/index");
+const app = express();
+const db = require("./db_config/sequelize");
+
+db.authenticate().then(() => {});
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.resolve(__dirname, "build")));
+
+//all the routes are here
+
+app.use("/distance", require("../server/routes/distance_routes"));
+app.use("/vehicletype", require("../server/routes/vehicletype_routes"));
+app.use("/fueltype", require("../server/routes/fueltype_routes"));
+app.use("/api", indexRouter);
+app.get("*", (req, res) => {
+  res.sendFile("build/index.html", { root: __dirname });
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// TODO Web Template Studio: Add your own error handler here.
+if (process.env.NODE_ENV === "production") {
+  // Do not send stack trace of error message when in production
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.send("Error occurred while handling the request.");
+  });
+} else {
+  // Log stack trace of error message while in development
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    console.log(err);
+    res.send(err.message);
+  });
+}
+
+module.exports = app;
